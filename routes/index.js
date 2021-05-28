1663ca69f6ce2fb3e052265c88849c8a28155fd7
asomma <alfonso.somma@alten.it>
2021-05-28 17:48:33 +0200

    first commit

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/lists', function (req, res, next) {
+    res.json({
+        name: "Alfonso",
+        surname : "Somma"
+    });
+});
+
 module.exports = router;
